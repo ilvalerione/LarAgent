893125d840451203ddc21cf48f8118e208debf09
Valerio <ilvalerione@gmail.com>
2025-02-27 15:13:21 +0100

    Update laragent.php cofiguration file

--- config/laragent.php
+++ config/laragent.php
@@ -2,11 +2,20 @@
 
 // config for Maestroerror/LarAgent
 return [
+    /*
+     * Determine the driver to use at runtime to connect with an LLM.
+     */
     'default_driver' => \LarAgent\Drivers\OpenAi\OpenAiDriver::class,
+
+    /*
+     * Determine the strategy to retain messages between multiple LLM interactions.
+     */
     'default_chat_history' => \LarAgent\History\InMemoryChatHistory::class,
 
+    /*
+     * Define the configuration options drivers need to be properly instatiated.
+     */
     'providers' => [
-
         'default' => [
             'name' => 'openai',
             'api_key' => env('OPENAI_API_KEY'),
